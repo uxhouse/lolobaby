28ed60acf34013fa6777ea53c9dfe74c476fe5a7
codestickdev <35964064+codestickdev@users.noreply.github.com>
2021-06-18 13:50:38 +0200

    Update _include_productTile-related.php

--- template-parts/_include_productTile-related.php
+++ template-parts/_include_productTile-related.php
@@ -10,14 +10,14 @@
         <?php endif; ?>
         <a href="<?php the_permalink(); ?>" class="productTile__cover">
             <h3><?php the_title(); ?></h3>
-            <?php if ( $price_html = $post->get_price_html() ) : ?>
+            <?php if ( $price_html = $product->get_price_html() ) : ?>
                 <p class="price"><?php echo $price_html; ?></p>
             <?php endif; ?>
             <p href="<?php the_permalink(); ?>" class="btn"><span>Sprawdź</span></p>
         </a>
         <a href="<?php the_permalink(); ?>" class="productTile__content">
             <h3><?php the_title(); ?></h3>
-            <?php if ( $price_html = $post->get_price_html() ) : ?>
+            <?php if ( $price_html = $product->get_price_html() ) : ?>
                 <p class="price"><?php echo $price_html; ?></p>
             <?php endif; ?>
             <p href="<?php the_permalink(); ?>" class="btn"><span>Zobacz</span></p>
